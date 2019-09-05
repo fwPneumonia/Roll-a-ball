bb62e0cc16954308a7ca23ed6eb0bc6dc33a90e9
fwPneumonia <37069126+fwPneumonia@users.noreply.github.com>
2019-09-05 21:32:40 +0300

    Add files via upload

---
+++ BallControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallControl : MonoBehaviour {
+    Rigidbody physic;
+    public int velocity;
+    int counter = 0;
+    public Text countertext;
+
+    public Text gameoverText;
+    public int collectingobjectnumber;
+	// Use this for initialization
+	void Start ()
+    {
+        physic = GetComponent<Rigidbody>();
+        countertext.text = "Counter = 0";
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 vec = new Vector3(horizontal, 0, vertical);
+        physic.AddForce(vec*velocity);
+
+
+        //Debug.Log("yatay= "+yatay+"    dikey = "+dikey);
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "ObjectToBeCollected") { }
+        other.gameObject.SetActive(false);
+        counter++;
+        countertext.text = "Counter=" + counter;
+        if (counter == collectingobjectnumber)
+        {
+            gameoverText.text = "Game Over!";
+        }
+    }
+    }
+
---
+++ CameraControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public GameObject Ball;
+    Vector3 distancebetween;
+	void Start ()
+    {
+        distancebetween = transform.position - Ball.transform.position;
+	}
+	
+	
+	void LateUpdate ()
+    {
+        transform.position = Ball.transform.position + distancebetween;
+
+	}
+}
